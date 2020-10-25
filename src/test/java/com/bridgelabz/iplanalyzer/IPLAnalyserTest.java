6f9ff7a8ebbbba437b0a8a80fa84310a33023ba4
arjun17697 <arjun17697@gmail.com>
2020-10-25 21:35:03 +0530

    Add:Test case and method to check top batting average

---
+++ src/test/java/com/bridgelabz/iplanalyzer/IPLAnalyserTest.java
@@ -0,0 +1,33 @@
+package com.bridgelabz.iplanalyzer;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.bridgelabz.principleofdesign.service.AnalyserException;
+import com.opencsv.exceptions.CsvException;
+
+/**
+ * Unit test for simple App.
+ */
+public class IPLAnalyserTest 
+{
+	private static final String IPL_BATTING = "C:/Users/HP LAP/Desktop/BridgeLabz/IPLAnalyzer/iplanalyzer/src/resources/WP DP Data_01 IPL2019FactsheetMostRuns.csv";
+	IPLAnalyser iplAnalyser;
+	
+	@Before
+	public void initialzier() {
+		iplAnalyser=new IPLAnalyser();
+	}
+	
+    @Test
+    public void givenIPLDataShouldReturnNoOfRecords() throws AnalyserException, CsvException
+    {
+    	List<IPLBatting> highestBattingAvg=iplAnalyser.sortByBattingAvgDesc(IPL_BATTING);
+    	assertEquals("83.2",highestBattingAvg.get(0).average);
+    }
+}
